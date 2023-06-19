4ffae277abad62980964f3b8bf8cd5c18cb897df
axealan <44321538+axealan@users.noreply.github.com>
2023-06-19 12:31:20 -0300

    Update scrap.js

--- src/scrap.js
+++ src/scrap.js
@@ -7,49 +7,47 @@ app.use(express.json());
 
 const TARGET = 'https://backoffice.minesbet.com/';
 
-app.get('/', async (req, res) => {
-
-    let email = req.body?.email;
-    let password = req.body?.password;
-
-    let player = req.body?.player;
-    let dateFrom = req.body?.dateFrom;
-    let dateTo = req.body?.dateTo;
-    let period  = req.body?.period;
+app.post('/', async (req, res) => {
+    const { email, password, player, dateFrom, dateTo, period } = req.body;
 
     if (!email || !password) {
-        res.send('Email or password not found');
+        res.send('Email ou senha não encontrados');
         return;
     }
 
-    result = await simpleScrap(email, password, player, dateFrom, dateTo, period);
-    res.send(result);
-
+    try {
+        const result = await simpleScrap(email, password, player, dateFrom, dateTo, period);
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao executar o scraping');
+    }
 });
 
 async function simpleScrap(email, password, player, dateFrom, dateTo, period) {
-
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-  
+
     await page.goto(`${TARGET}login`);
-  
-    await page.setViewport({width: 1080, height: 1024});
+
+    await page.setViewport({ width: 1080, height: 1024 });
     await page.waitForSelector('.form');
 
     await page.evaluate((email) => {
-        document.querySelector('input[name="email"]').value = email
+        document.querySelector('input[name="email"]').value = email;
     }, email);
 
     await page.evaluate((password) => {
-        document.querySelector('input[type="password"]').value = password
+        document.querySelector('input[type="password"]').value = password;
     }, password);
-    
+
     await page.evaluate(() => {
         document.querySelector('button').click();
     });
 
-    const danger = await page.waitForSelector('.alert-danger', {timeout: 1000}).then(() => true).catch(() => false);
+    const danger = await page.waitForSelector('.alert-danger', { timeout: 1000 })
+        .then(() => true)
+        .catch(() => false);
 
     if (danger) {
         const error = await page.evaluate(() => {
@@ -59,32 +57,32 @@ async function simpleScrap(email, password, player, dateFrom, dateTo, period) {
             return li.innerText;
         });
 
-        if (error?.length > 0) {
-            await browser.close();
-            console.log('Erro ao fazer login');
-            return {
-                email: email,
-                password: password,
-                error: error
-            }
-        }
+        await browser.close();
+
+        console.log('Erro ao fazer login');
+
+        return {
+            email: email,
+            password: password,
+            error: error
+        };
     }
 
     await page.waitForNavigation();
-    
-    response = await openReport(browser, page, player, dateFrom, dateTo, period);
+
+    const response = await openReport(browser, page, player, dateFrom, dateTo, period);
+
+    await browser.close();
 
     return response;
-};
+}
 
 async function openReport(browser, page, player, dateFrom, dateTo, period) {
-
     await page.goto(`${TARGET}admin/affiliates/19/players?player=${player}&dateFrom=${dateFrom}&dateTo=${dateTo}&list=&period=${period}`);
-    await page.setViewport({width: 1080, height: 1024});
+    await page.setViewport({ width: 1080, height: 1024 });
     await page.waitForSelector('.table');
 
     const report = await page.evaluate(() => {
-
         const table = document.querySelector('.table');
 
         let response = [];
@@ -95,32 +93,27 @@ async function openReport(browser, page, player, dateFrom, dateTo, period) {
         const tbody = table.querySelector('tbody');
 
         const trs = tbody.querySelectorAll('tr');
-        const trsText = Array.from(trs).map(tr => {
+        Array.from(trs).forEach(tr => {
             const tds = tr.querySelectorAll('td');
             const tdsText = Array.from(tds).map(td => td.innerText);
 
             response.push(
-                Object.assign(...tdsText.map((td, i) => ({[thsText[i]]: td})))
+                Object.fromEntries(tdsText.map((td, i) => [thsText[i], td]))
             );
-            
         });
 
-        return response;
-
+        return {
+            player: player,
+            dateFrom: dateFrom,
+            dateTo: dateTo,
+            period: period,
+            report: response
+        };
     });
 
-    await browser.close();
-
-    return {
-        player: player,
-        dateFrom: dateFrom,
-        dateTo: dateTo,
-        period: period,
-        report: report
-    }
-    
+    return report;
 }
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('Aplicação de exemplo ouvindo na porta 3000!');
 });
